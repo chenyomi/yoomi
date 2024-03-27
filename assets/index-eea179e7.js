fb85af9eeff994d64c1d6cac23536ee9567f0b31
chenyomi <chenyomi@users.noreply.github.com>
2024-03-27 03:14:31 +0000

    deploy: efc90f14b1a220391d1c59bbcd246a312703d741

---
+++ assets/index-eea179e7.js
@@ -0,0 +1,1 @@
+import{bv as e}from"./index-31b9b4d3.js";const r=e("store",{state:()=>({view:!0,message:null,process:{}}),actions:{setMessage(s){this.message=s},setProcess(s){this.process=Object.assign(this.process,s)}}});export{r as u};
